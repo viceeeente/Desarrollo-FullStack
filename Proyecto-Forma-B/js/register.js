5e960ecd2641419979df24f60c9e54384e7f4a97
vice. <vice.alvarezm@duocuc.cl>
2025-09-04 22:58:20 -0400

    Continuación de verificaciones de campos de el formulario, pero siguen sin funcionar

--- Proyecto-Forma-B/js/register.js
+++ Proyecto-Forma-B/js/register.js
@@ -1,4 +1,4 @@
-const name = document.getElementById("name");
+const names = document.getElementById("name");
 const user = document.getElementById("user");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
@@ -8,18 +8,18 @@ const errors = document.getElementById("errors");
 
 const set = new Set();
 
-name.addEventListener('blur' ,function(e) {
-    if(name.value.lenght == 0){
-        name.classList.add("errors");
+names.addEventListener('blur' ,function(e) {
+    if(names.value.lenght == 0){
+        names.classList.add("errors");
         set.add("<p>Completa el campo Nombre</p>");
     }else{
-        name.classList.remove("errors");
+        names.classList.remove("errors");
     }
 });
 user.addEventListener('blur' ,function(e) {
     if(user.value.lenght == 0){
         user.classList.add("errors");
-        set.add("<p>Completa el campo Nombre</p>");
+        set.add("<p>Completa el campo User</p>");
     }else{
         user.classList.remove("errors");
     }
@@ -27,8 +27,29 @@ user.addEventListener('blur' ,function(e) {
 email.addEventListener('blur' ,function(e) {
     if(!email.value.includes("@")){
         email.classList.add("errors");
-        set.add("<p>Completa el campo Nombre</p>");
+        set.add("<p>Completa el campo Correo</p>");
     }else{
         email.classList.remove("errors");
     }
 });
+password2.addEventListener('blur' ,function(e) {
+    if(!password.value!=password2.value){
+        email.classList.add("errors");
+        set.add("<p>Las contraseñas no coinciden</p>");
+    }else{
+        email.classList.remove("errors");
+        set.add("<p>Las contraseñas coinciden")
+    }
+});
+
+form.addEventListener('submit', function (e){
+    errors.innerHTML = "";
+    document.querySelectorAll("input").forEach(element=> {
+        if(element.classList.contains("error")){
+            e.preventDefault();
+            set.forEach(p=>{
+                errors.innerHTML +=p;
+            })
+        }
+    })
+})
