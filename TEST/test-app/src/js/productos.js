cbf59fb961058bb66eaaaadfa37f4766b6aa0f6d
vice. <vice.alvarezm@duocuc.cl>
2025-09-27 16:03:13 -0300

    Lista de productos, se agregó image y description

---
+++ TEST/test-app/src/js/productos.js
@@ -0,0 +1,22 @@
+export const productos = [
+  { id: "CO001", categoria: "Consolas", nombre: "PlayStation 5", precio: 549990 , image: "https://www.esrb.org/wp-content/uploads/2024/09/slide_playstation-5.png", description: "La consola de última generación de Sony, que ofrece gráficos impresionantes y tiempos de carga ultrarrápidos para una experiencia de juego inmersiva"},
+  { id: "CO002", categoria: "Consolas", nombre: "Xbox Series X", precio: 499990 , image: "https://media.solotodo.com/media/nav_items/xboxx.png" , description: "La consola más potente de Microsoft con soporte para 4K, Game Pass y cientos de títulos disponibles."},
+
+  { id: "CG001", categoria: "Computadores Gamers", nombre: "PC Gamer ASUS ROG Strix", precio: 1299990 },
+  { id: "CG002", categoria: "Computadores Gamers", nombre: "PC Gamer AMD Ryzen", precio:  1100000 },
+  
+  { id: "JM001", categoria: "Juegos de Mesa", nombre: "Catan", precio: 29990 },
+  { id: "JM002", categoria: "Juegos de Mesa", nombre: "Carcassonne", precio: 24990 },
+  
+  { id: "AC001", categoria: "Accesorios", nombre: "Controlador Inalámbrico Xbox Series X", precio: 59990 },
+  { id: "AC002", categoria: "Accesorios", nombre: "Auriculares Gamer HyperX Cloud II", precio: 79990 },
+  
+  { id: "SG001", categoria: "Sillas Gamers", nombre: "Silla Gamer SecretLab Titan", precio: 349990 },
+  
+  { id: "MS001", categoria: "Mouse", nombre: "Mouse Gamer Logitech G502 HERO", precio: 49990 },
+
+  { id: "MP001", categoria: "Mousepad", nombre: "Mousepad Razer Goliathus Extended Chroma", precio: 29990 },
+  
+  { id: "PP001", categoria: "Poleras Personalizadas", nombre: "Polera Gamer Personalizada 'Level-UP'", precio: 14990 },
+
+];
