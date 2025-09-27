7af921728544d040126ac507da72318de67d5bc6
vice. <vice.alvarezm@duocuc.cl>
2025-09-27 16:09:17 -0300

    Modificación de la ruta del import de Footer

--- TEST/test-app/src/App.js
+++ TEST/test-app/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Homepage from './pages/Homepage';  
 import ConsolePage from './pages/Consoles';
-import Footer from './pages/Footer';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
