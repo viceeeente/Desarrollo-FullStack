6b39e112b93edc5c438368a77dfe7a1fec760dbc
vice. <vice.alvarezm@duocuc.cl>
2025-09-27 16:06:40 -0300

    Modificación de la obtención de datos de los productos y Link del back-button

--- TEST/test-app/src/pages/Consoles.jsx
+++ TEST/test-app/src/pages/Consoles.jsx
@@ -1,17 +1,47 @@
 import React from "react";
 import './Consoles.css';
+import { Link } from "react-router-dom";
+import { productos } from '../js/productos'
 
 const Consoles = () => {
 
+    const names = ["PlayStation 5","Xbox Series X"];
+
+    const filtredProducts = productos.filter(producto => 
+        names.includes(producto.nombre)
+    );
+
     return (
         <div className="consoled">
-            <section className="categories">
-                <div className="categories-text">
-                    <h2>Consolas</h2>
+            <section class="products-section">
+                <h2>Consolas</h2>
+                <div className="products-container">
+          {filtredProducts.map((producto) => (
+            <div className="product-card" key={producto.id}>
+              <div className="card-inner">
+                <div className="card-front">
+                  <img src={producto.image} alt={producto.nombre} />
+                  <h3>{producto.nombre}</h3>
+                  <p>${producto.precio}</p>
                 </div>
-            </section>
-    
+                <div className="card-back">
+                  <h3>{producto.nombre}</h3>
+                  <p>{producto.description}</p>
+                  <button className="add-to-cart" data-id={producto.id}>
+                    Agregar al carrito
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
+        <Link to ="/">
+        <div class="back-button">
+            <a>Volver al menú principal</a>    
+        </div>
+        </Link>
+        </section>
+    </div>
     );
 };
 
