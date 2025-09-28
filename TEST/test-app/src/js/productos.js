ac38398363dd201ff61f3b3a6ee77ae5f90da206
vice. <vice.alvarezm@duocuc.cl>
2025-09-27 22:04:31 -0300

    Agregado de image y description a productos con id : CG001 y CG002

--- TEST/test-app/src/js/productos.js
+++ TEST/test-app/src/js/productos.js
@@ -2,8 +2,8 @@ export const productos = [
   { id: "CO001", categoria: "Consolas", nombre: "PlayStation 5", precio: 549990 , image: "https://www.esrb.org/wp-content/uploads/2024/09/slide_playstation-5.png", description: "La consola de última generación de Sony, que ofrece gráficos impresionantes y tiempos de carga ultrarrápidos para una experiencia de juego inmersiva"},
   { id: "CO002", categoria: "Consolas", nombre: "Xbox Series X", precio: 499990 , image: "https://media.solotodo.com/media/nav_items/xboxx.png" , description: "La consola más potente de Microsoft con soporte para 4K, Game Pass y cientos de títulos disponibles."},
 
-  { id: "CG001", categoria: "Computadores Gamers", nombre: "PC Gamer ASUS ROG Strix", precio: 1299990 },
-  { id: "CG002", categoria: "Computadores Gamers", nombre: "PC Gamer AMD Ryzen", precio:  1100000 },
+  { id: "CG001", categoria: "Computadores Gamers", nombre: "PC Gamer ASUS ROG Strix", precio: 1299990, image:"https://m.media-amazon.com/images/I/718F-FqoEaL.jpg", description: "Un potente equipo diseñado para los gamers más exigentes, equipado con los últimos componentes para ofrecer un rendimiento excepcional encualquier juego." },
+  { id: "CG002", categoria: "Computadores Gamers", nombre: "PC Gamer AMD Ryzen", precio:  1100000 , image: "https://ecoindigital.cl/cdn/shop/files/PCGAMER_2048x2048.png?v=1712175506", description:"Equipado con procesador Ryzen 7, tarjeta gráfica RTX 3070, 16GB RAM y excelente relación calidad-precio."},
   
   { id: "JM001", categoria: "Juegos de Mesa", nombre: "Catan", precio: 29990 },
   { id: "JM002", categoria: "Juegos de Mesa", nombre: "Carcassonne", precio: 24990 },
