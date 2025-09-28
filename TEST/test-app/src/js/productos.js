56a32a4affb682acf9dd6b7cff08b42a87cbd139
vice. <vice.alvarezm@duocuc.cl>
2025-09-27 22:36:46 -0300

    Agregado de image y description a productos faltantes de estas características

--- TEST/test-app/src/js/productos.js
+++ TEST/test-app/src/js/productos.js
@@ -5,18 +5,18 @@ export const productos = [
   { id: "CG001", categoria: "Computadores Gamers", nombre: "PC Gamer ASUS ROG Strix", precio: 1299990, image:"https://m.media-amazon.com/images/I/718F-FqoEaL.jpg", description: "Un potente equipo diseñado para los gamers más exigentes, equipado con los últimos componentes para ofrecer un rendimiento excepcional encualquier juego." },
   { id: "CG002", categoria: "Computadores Gamers", nombre: "PC Gamer AMD Ryzen", precio:  1100000 , image: "https://ecoindigital.cl/cdn/shop/files/PCGAMER_2048x2048.png?v=1712175506", description:"Equipado con procesador Ryzen 7, tarjeta gráfica RTX 3070, 16GB RAM y excelente relación calidad-precio."},
   
-  { id: "JM001", categoria: "Juegos de Mesa", nombre: "Catan", precio: 29990 },
-  { id: "JM002", categoria: "Juegos de Mesa", nombre: "Carcassonne", precio: 24990 },
+  { id: "JM001", categoria: "Juegos de Mesa", nombre: "Catan", precio: 29990, image:"https://x.boardgamearena.net/data/gamemedia/catan/box/en_280.png?h=1751536970", description:"Un clásico juego de estrategia donde los jugadores compiten por colonizar y expandirse en la isla de Catan. Ideal para 3-4 jugadores y perfecto para noches de juego en familia o con amigos" },
+  { id: "JM002", categoria: "Juegos de Mesa", nombre: "Carcassonne", precio: 24990, image:"https://cdnx.jumpseller.com/juegos-gori/image/50813062/resize/1200/1200?1728589746", description:"Un juego de colocación de fichas donde los jugadores construyen el paisaje alrededor de la fortaleza medieval de Carcassonne. Ideal para 2-5 jugadores y fácil de aprender." },
   
-  { id: "AC001", categoria: "Accesorios", nombre: "Controlador Inalámbrico Xbox Series X", precio: 59990 },
-  { id: "AC002", categoria: "Accesorios", nombre: "Auriculares Gamer HyperX Cloud II", precio: 79990 },
+  { id: "AC001", categoria: "Accesorios", nombre: "Controlador Inalámbrico Xbox Series X", precio: 59990, image:"https://goldengamers.cl/cdn/shop/products/1_91_1200x1200.png?v=1650388642", description:"Ofrece una experiencia de juego cómoda con botones mapeables y una respuesta táctil mejorada. Compatible con consolas Xbox y PC." },
+  { id: "AC002", categoria: "Accesorios", nombre: "Auriculares Gamer HyperX Cloud II", precio: 79990, image:"https://media.spdigital.cl/thumbnails/products/4uskcpud_85117974_thumbnail_512.png", description:"Proporcionan un sonido envolvente de calidad con un micrófono desmontable y almohadillas de espuma viscoelástica para mayor comodidad durante largas sesiones de juego." },
   
-  { id: "SG001", categoria: "Sillas Gamers", nombre: "Silla Gamer SecretLab Titan", precio: 349990 },
+  { id: "SG001", categoria: "Sillas Gamers", nombre: "Silla Gamer SecretLab Titan", precio: 349990, image:"https://images.secretlab.co/theme/common/teamsecret-titanevo2022-chairs-min.png", description:"La Silla Gamer SecretLab Titan ofrece máxima comodidad y ergonomía, con soporte lumbar ajustable, reposabrazos 4D y respaldo reclinable." },
   
-  { id: "MS001", categoria: "Mouse", nombre: "Mouse Gamer Logitech G502 HERO", precio: 49990 },
+  { id: "MS001", categoria: "Mouse", nombre: "Mouse Gamer Logitech G502 HERO", precio: 49990, image:"https://cdnx.jumpseller.com/easytech-store/image/24090184/resize/1200/1200?1652376809", description:"Mouse Logitech G502 HERO con sensor HERO 25K, 11 botones programables y pesos ajustables." },
 
-  { id: "MP001", categoria: "Mousepad", nombre: "Mousepad Razer Goliathus Extended Chroma", precio: 29990 },
+  { id: "MP001", categoria: "Mousepad", nombre: "Mousepad Razer Goliathus Extended Chroma", precio: 29990, image:"https://dl.razerzone.com/src2/eeimages/1395/1395_goliathusextended.png", description:"Mousepad Razer Goliathus Extended Chroma con iluminación RGB y superficie optimizada para precisión y velocidad" },
   
-  { id: "PP001", categoria: "Poleras Personalizadas", nombre: "Polera Gamer Personalizada 'Level-UP'", precio: 14990 },
+  { id: "PP001", categoria: "Poleras Personalizadas", nombre: "Polera Gamer Personalizada 'Level-UP'", precio: 14990, image:"ttps://m.media-amazon.com/images/I/B1pppR4gVKL._CLa%7C2140%2C2000%7C91ISC-C9YRL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_UY1000_.png", description:"La Polera Gamer Level-Up es cómoda, moderna y personalizada, ideal para jugadores que quieren estilo y identidad en su look." },
 
 ];
