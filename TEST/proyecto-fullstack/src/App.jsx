e28fcf19523951b67c13a51c3f56f5ef2bad5840
vice. <vice.alvarezm@duocuc.cl>
2025-10-08 11:00:39 -0300

    test #02

--- TEST/proyecto-fullstack/src/App.jsx
+++ TEST/proyecto-fullstack/src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Homepage from './pages/HomePage';  
-import ConsolePage from './pages/Consoles';
-import PcPage from './pages/PC';
-import Games from './pages/Games';
+import Homepage from './pages/HomePage/HomePage';  
+import ConsolePage from './pages/Consoles/Consoles';
+import PcPage from './pages/PC/PC';
+import Games from './pages/Games/Games';
 import './App.css';
 
 function App() {
