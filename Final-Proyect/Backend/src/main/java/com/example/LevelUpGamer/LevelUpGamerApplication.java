d6a817a97ad4a3848f90ed6ad96be335714301e4
Samuel-Gallardo <sa.gallardov@duocuc.cl>
2025-11-02 23:33:54 -0300

    ''test

---
+++ Final-Proyect/Backend/src/main/java/com/example/LevelUpGamer/LevelUpGamerApplication.java
@@ -0,0 +1,13 @@
+package com.example.LevelUpGamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpGamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelUpGamerApplication.class, args);
+	}
+
+}
---
+++ Final-Proyect/Backend/src/test/java/com/example/LevelUpGamer/LevelUpGamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LevelUpGamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpGamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
